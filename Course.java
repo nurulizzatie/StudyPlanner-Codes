fd1cb6b67c6c3b506ed23c3b470ea970b4c6b22b
nurulizzatie <126445328+nurulizzatie@users.noreply.github.com>
2023-11-16 21:57:48 +0800

    Create Course.java

---
+++ Course.java
@@ -0,0 +1,13 @@
+package com.StudyPlanner2;
+public class Course {
+    private String courseName;
+    private String instructor;
+
+    public Course(String courseName, String instructor) {
+        this.courseName = courseName;
+        this.instructor = instructor;
+    }
+
+    // Getters and setters
+    // ...
+}
